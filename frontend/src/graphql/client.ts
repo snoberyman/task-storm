e058a25ae4271586662dd773dea5e7132c55605a
snoberyman <yaman.sanobar@gmail.com>
2025-12-14 11:09:41 -0800

    linking front-end with server

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -2,9 +2,8 @@ import { GraphQLClient } from "graphql-request";
 
 const url = import.meta.env.VITE_GRAPHQL_URL;
 if (!url) {
-  throw new Error(
-    "VITE_GRAPHQL_URL is not defined. Check your .env file or Netlify env vars."
-  );
+  console.error("VITE_GRAPHQL_URL is not defined");
+  throw new Error("VITE_GRAPHQL_URL is not defined");
 }
 
 export const client = new GraphQLClient(url);
